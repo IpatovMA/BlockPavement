9b114c687be4749ab9bffb052fdda5b92fa2a9a5
BuildTools <unconfigured@null.spigotmc.org>
2019-08-28 01:53:40 +0300

    в конце уровня камера крутится

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,85 +5,181 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public bool MenuCamera=false;
-    // public GameObject scaleTarget;
-    // public float width;
-    public float smoothTime = 5;
+    public float zoomSpeed = 5;
     public float offset = 0f;
 
     private Camera cam;
-    private Vector3 scaler;
+    private Transform CameraAlign;
     public LevelStageData Stage;
     public float PlayCamHeight ;
+    public float MenuCamHeight ;
+
+    public float[] loopSpeed;
+    public float changeLoopSpeedFactor;
+    public float loopRadius;
+    public float rotationStartRadius;
+    public float viewAngel;
+    public int fastLoopNum = 2;
     private Vector3 floatPos;
-    private Vector3 MenuCamPos;
+    // public Vector3[] MenuCamPos = new Vector3[2];
+    private bool aligned=false;
+    private float radius;
+    private int loopCounter;
+    private float currentLoopSpeed;
+
+    // private bool RotateBool;// 
     void Start(){
         cam = GetComponent<Camera>();
+        CameraAlign = transform.parent.transform;
+
     }
     void FixedUpdate()
     {
-            // target = LevelSpawner.Level.transform.Find("Player").transform;
-
          if (target == null) return;
-         if (Stage == null||LevelManager.State ==LevelManager.lvlState.Menu){
-        Stage = target.GetComponent<LevelStageData>();
-              if (Stage != null){  
-                floatPos = new Vector3(Stage.MapWidth/2.0f,Stage.MapHeight/2.0f,0);
-                 MenuCamPos = transform.position;
-
-                // PlayCamAngels = new Vector3()
-              }
-
-         }
-
+  
+            Debug.Log(loopCounter);
 
             switch(LevelManager.State){
                 case LevelManager.lvlState.Menu:
-                 transform.position =  new Vector3(target.position.x + offset+floatPos.x, transform.position.y, transform.position.z);
-                // MoveTo(new Vector3(target.position.x + offset+floatPos.x, transform.position.y, transform.position.z));
-                ScaleWithFactor(16);
+                    
+                
+                        Stage = target.GetComponent<LevelStageData>();
+                        viewAngel = transform.eulerAngles.x;
+                        loopCounter = 0;
 
+                    if (Stage != null){  
+                        floatPos = new Vector3(Stage.MapWidth/2.0f,Stage.MapHeight/2.0f,0);
+                        transform.position =  new Vector3(0, -loopRadius, 0)+CameraAlign.position;
+                        CameraAlign.position = target.position+floatPos+new Vector3(0,0,MenuCamHeight);
+                        CameraAlign.eulerAngles = Vector3.zero;
+                        loopRadius = Stage.MapHeight>Stage.MapWidth ? Stage.MapHeight : Stage.MapWidth;
+                    }
+                
+                    ScaleWithFactor(16);
+                    transform.LookAt(target.position+floatPos);
                 break;
                 case LevelManager.lvlState.Play:
-                transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x + offset+floatPos.x, target.position.y+floatPos.y, PlayCamHeight), Time.deltaTime * smoothTime);
-                // MoveTo(new Vector3(target.position.x + offset+floatPos.x, target.position.y+floatPos.y, PlayCamHeight));
-                // transform.eulerAngles= Vector3.Lerp(transform.eulerAngles, new Vector3(0,0,90), Time.deltaTime * smoothTime);
-                ScaleWithFactor(13);
-
+                    transform.position = Vector3.Lerp(transform.position,CameraAlign.position, Time.deltaTime * zoomSpeed);
+                    HeightChange(PlayCamHeight);
+                    ScaleWithFactor(13);
+                    transform.LookAt(target.position+floatPos);
                 break;
                 case LevelManager.lvlState.Fin:
-                transform.position = Vector3.Lerp(transform.position, MenuCamPos, Time.deltaTime * smoothTime);
-                ScaleWithFactor(16);
+                    radius = Mathf.Lerp(radius, loopRadius,Time.deltaTime * zoomSpeed);
+                    transform.localPosition  = Vector3.Lerp(transform.localPosition, new Vector3(0,-radius,0), Time.deltaTime * zoomSpeed);
+
+                    HeightChange(MenuCamHeight);
+                    // makeLoop(radius);
+                    if (radius < rotationStartRadius){
+                    transform.LookAt(target.position+floatPos);
+
+                    }else{
+                        if(loopCounter<fastLoopNum){
+                            SmoothlChangeSpeed(0);
+                        } else{
+                            SmoothlChangeSpeed(1);
+                        }
+
+                        makeLoop(radius);
+
+                    }
+                    // makeLoop(loopRadius);
+                    // transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, MenuCamPos[1], Time.deltaTime * zoomSpeed);
+                    // Debug.Log(currentLoopSpeed);
+                    ScaleWithFactor(16);
+                    // makeLoop(loopRadius);
                 break;
 
             }
 
-                transform.LookAt(target.position+floatPos);
            
     }
 
  void ScaleWithFactor(float factor){
-     scaler = Vector3.Lerp(new Vector3(0,0,GetComponent<Camera>().fieldOfView), new Vector3(0,0,Stage.MapWidth*factor), Time.deltaTime * smoothTime);
-     cam.fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, Stage.MapWidth*factor, Time.deltaTime * smoothTime); 
+    //  scaler = Vector3.Lerp(new Vector3(0,0,GetComponent<Camera>().fieldOfView), new Vector3(0,0,Stage.MapWidth*factor), Time.deltaTime * zoomSpeed);
+     cam.fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, Stage.MapWidth*factor, Time.deltaTime * zoomSpeed); 
  }
+void HeightChange(float height){
+    CameraAlign.position=Vector3.Lerp(CameraAlign.position,new Vector3(CameraAlign.position.x,CameraAlign.position.y,height), Time.deltaTime * zoomSpeed);
+}
 
-    void MoveTo(Vector3 pos){
-        float delta=Time.deltaTime*smoothTime; 
-        if(transform.position != pos){
-            if(transform.position.x != pos.x){
-                if (transform.position.x < pos.x) {transform.Translate(delta,0,0);}
-                    else{transform.Translate(-delta,0,0);}
-            }
-            if(transform.position.y != pos.y){
-                if (transform.position.y < pos.y) {transform.Translate(0,delta,0);}
-                    else{transform.Translate(0,-delta,0);}
-            }  
-            if(transform.position.z != pos.z){ 
-                if (transform.position.z < pos.z) {transform.Translate(0,0,delta);}
-                    else{transform.Translate(0,0,-delta);}
-            }
+    void makeLoop(float rad){
+        // // alpha = Mathf.Lerp(alpha,270,Time.deltaTime*currentLoopSpeed);
+        float delta  = Time.deltaTime*currentLoopSpeed;
+        // Debug.Log(CameraAlign.localEulerAngles+"  "+CameraAlign.eulerAngles);
+        // Debug.Log(Mathf.Pow(transform.localPosition.x,2)+Mathf.Pow(transform.localPosition.y,2)-Mathf.Pow(rad,2));
+// Debug.Log(transform.localEulerAngles.x +"  "+ viewAngel);
+
+        // if(rad<loopRadius){
+        //     rad = Mathf.Lerp(rad,loopRadius,Time.deltaTime*zoomSpeed);
+        //     transform.localPosition = new Vector3(0,rad,0);
+        // }
+        // if(transform.localEulerAngles.x > viewAngel||transform.localEulerAngles.x<0.1f){
+            // transform.localEulerAngles = new Vector3(transform.localEulerAngles.x - Time.deltaTime*zoomSpeed*10,0,0);
+            if (transform.localEulerAngles.x<0.1f) transform.localEulerAngles = new Vector3(-delta,0,0);
+            transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles,new Vector3(viewAngel,0,0),Time.deltaTime*zoomSpeed);
+        // }
+        CameraAlign.Rotate(new Vector3(0,0,delta));
+
+        if (CameraAlign.eulerAngles.z < delta-delta/10){
+            loopCounter++;
+            // Debug.Log(loopCounter);
         }
+        
+    //     if (Mathf.Pow(transform.localPosition.x,2)+Mathf.Pow(transform.localPosition.y,2)-Mathf.Pow(rad,2)<0f){
+    //    Debug.Log("dsadsa");
+    //         float ang = transform.localEulerAngles.z;
+    //         // transform.localPosition = new Vector3(transform.localPosition.x-Mathf.Cos(ang),transform.localPosition.y-Mathf.Sin(ang),0);
+    //         transform.Translate(-Mathf.Cos(ang),-Mathf.Sin(ang),0,Space.Self);
+    //     }
+                
+
+        // // float deltaAng = viewAngel*delta/90f;
+
+        // alpha = 270-alpha>delta ? alpha+delta : -90;
+
+        // float x = target.position.x + floatPos.x + rad*Mathf.Cos(alpha*Mathf.PI/180f);
+        // float y = target.position.y + floatPos.y + rad*Mathf.Sin(alpha*Mathf.PI/180f);
+        
+        // float xAng =transform.eulerAngles.x;
+        // float yAng =transform.eulerAngles.y;
+        
+        // // xAng = alpha<90 ? xAng+deltaAng : xAng-deltaAng;
+        // // yAng = alpha>0&&alpha<180 ? yAng+deltaAng : yAng-deltaAng;
+
+        // transform.position = new Vector3(x,y,transform.position.z);
+        // transform.eulerAngles = new Vector3(xAng,yAng,alpha+zoomSpeed);
+        // // transform.eulerAngles = new Vector3(-viewAngel*Mathf.Sin(alpha*Mathf.PI/180f),viewAngel*Mathf.Cos(alpha*Mathf.PI/180f),alpha+90);
+
+        // // transform.Rotate(new Vector3(deltaAng,-deltaAng,delta));
+        // // transform.rotation.Set(xAng,yAng,alpha+90,currentLoopSpeed);
+        // // Debug.DrawLine(DebugPos, transform.position, Color.red,60);
+
+
+
     }
+
+    void SmoothlChangeSpeed(int i){
+        currentLoopSpeed = Mathf.Lerp(currentLoopSpeed,loopSpeed[i],Time.deltaTime*changeLoopSpeedFactor);
+    }
+
+    // void MoveTo(Vector3 pos){
+    //     float delta=Time.deltaTime*zoomSpeed; 
+    //     if(transform.position != pos){
+    //         if(transform.position.x != pos.x){
+    //             if (transform.position.x < pos.x) {transform.Translate(delta,0,0);}
+    //                 else{transform.Translate(-delta,0,0);}
+    //         }
+    //         if(transform.position.y != pos.y){
+    //             if (transform.position.y < pos.y) {transform.Translate(0,delta,0);}
+    //                 else{transform.Translate(0,-delta,0);}
+    //         }  
+    //         if(transform.position.z != pos.z){ 
+    //             if (transform.position.z < pos.z) {transform.Translate(0,0,delta);}
+    //                 else{transform.Translate(0,0,-delta);}
+    //         }
+    //     }
+    // }
 
 }
 
@@ -114,4 +210,4 @@ public class CameraFollow : MonoBehaviour
 //     }
 // }
 
-// Quaternion.SetFromToRotation 
+// Quaternion.SetFromToRotation  
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,9 +8,11 @@ public class LevelManager : MonoBehaviour
 {
    public enum lvlState {Menu,Play,Fin};
     public GameObject StartPage;
+    public GameObject FinPage;
+
     public GameObject[] Levels;
     public int lvlNum= 0;
-     [SerializeField]public static lvlState State;
+    public static lvlState State;
      public GameObject player;
     // public Camera MenuCam;
     
@@ -28,22 +30,25 @@ public class LevelManager : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        if (State == lvlState.Menu&&!Levels[lvlNum].activeSelf){
-                        
+    {   
+        // Debug.Log(State+"  "+StartPage.activeSelf);
+        if (State == lvlState.Menu&&!StartPage.activeSelf){
+                        StartPage.SetActive(true);
 
             Camera.main.GetComponent<CameraFollow>().target = Levels[lvlNum].GetComponentInChildren<LevelStageData>().gameObject.transform;
+        //    if (Camera.main.GetComponent<CameraFollow>().target.position.x == Camera.main.transform.position.x){
+                // if (!player.activeSelf){
             Levels[lvlNum].SetActive(true);
+                // }            
             player = Levels[lvlNum].GetComponentInChildren<PlayerControl>().gameObject;
             player.SetActive(false);
-            StartPage.SetActive(true);
+            
+            // Debug.Log("fdsfdsf");
+            //}
         }
 
-        if(State == lvlState.Fin&&Input.GetMouseButtonDown(0)){
-                if(lvlNum==Levels.Length-1) SceneManager.LoadScene("game");
-            State = lvlState.Menu; 
-            Levels[lvlNum].SetActive(false);
-            lvlNum++;   
+        if(State == lvlState.Fin&&!FinPage.activeSelf){
+            FinPage.SetActive(true);
             // SceneManager.LoadScene("game");
         }
        
@@ -56,6 +61,14 @@ public class LevelManager : MonoBehaviour
 
         // Camera.main.gameObject.SetActive(true);
         // MenuCam.gameObject.SetActive(false);
+    }
+    public void GetRewards(){
+            if(lvlNum==Levels.Length-1) SceneManager.LoadScene("game");
+            State = lvlState.Menu; 
+            Levels[lvlNum].SetActive(false);
+            lvlNum++;   
+            FinPage.SetActive(false);
+
     }
 }
 
